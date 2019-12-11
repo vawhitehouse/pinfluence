1789d01fbe0a1fd08a79ddb223c487b8f5fc9e74
Valerie Whitehouse <vawhitehouse@yahoo.com>
2019-12-11 10:59:40 -0500

    session form styling

--- frontend/components/session/login.jsx
+++ frontend/components/session/login.jsx
@@ -31,24 +31,20 @@ class Login extends React.Component {
   render() {
 
     return (
-      <div>
+      <div className="session-container">
         <button><Link to='/signup'>Sign up</Link></button>
 
-        <div className="">
+        <div className="session-box">
           <img src="assets/pinfluence-logo-purple.png" height="45px" width="45px" alt="Pinfluence Logo" />
 
-          <h3>Welcome to Pinterest</h3>
+          <h3>Welcome to Pinfluence</h3>
 
           {this.renderErrors()}
 
-          <form onSubmit={this.handleSubmit}>
-            <br />
-            <input type="email" placeholder="Email" onChange={this.update('email')} />
-            <br />
-            <input type="password" placeholder="Password" onChange={this.update('password')} />
-            <br />
-            <input type="submit" value="Log in" />
-            <br />
+          <form onSubmit={this.handleSubmit} className="session-form">
+            <input className="session-form-input" type="email" placeholder="Email" onChange={this.update('email')} />
+            <input className="session-form-input" type="password" placeholder="Password" onChange={this.update('password')} />
+            <input className="session-form-submit" type="submit" value="Log in" />
             <p><Link to='/signup'>Not on Pinterest yet? Sign up</Link></p>
           </form>
 
--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -31,24 +31,25 @@ class Signup extends React.Component {
   render() {
 
     return (
-      <div>
-        <button><Link to='/login'>Log in</Link></button>
+      <div className="session-container">
+        <button className="session-top-button"><Link to='/login'>Log in</Link></button>
 
-        {this.renderErrors()}
+        <div className="session-box">
+          <img src="assets/pinfluence-logo-purple.png" height="45px" width="45px" alt="Pinfluence Logo" />
 
-        <form onSubmit={this.handleSubmit}>
-          <br/>
-          <input type="email" placeholder="Email" onChange={this.update('email')}/>
-          <br/>
-          <input type="password" placeholder="Password" onChange={this.update('password')}/>
-          <br/>
-          <input type="text" placeholder="Age" onChange={this.update('age')}/>
-          <br/>
-          <input type="submit" value="Continue"/>
-          <br/>
-          <p><Link to='/login'>Already a member? Log in</Link></p>
-        </form>
+          <h3>Welcome to Pinfluence</h3>
+          <p>Find new ideas to try</p>
+          {this.renderErrors()}
 
+          <form onSubmit={this.handleSubmit} className="session-form">
+            <input className="session-form-input" type="email" placeholder="Email" onChange={this.update('email')}/>
+            <input className="session-form-input" type="password" placeholder="Enter password" onChange={this.update('password')}/>
+            <input className="session-form-input" type="text" placeholder="Age" onChange={this.update('age')}/>
+            <input className="session-form-submit" type="submit" value="Continue" />
+            <p><Link to='/login'>Already a member? Log in</Link></p>
+          </form>
+
+        </div>
       </div>
     )
   }
