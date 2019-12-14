a318f14fc6225018156521c56c8d23714c927381
Valerie Whitehouse <vawhitehouse@yahoo.com>
2019-12-14 16:53:59 -0500

    nav bar work

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -17,12 +17,31 @@ export default ({ currentUser, logout }) => {
 
   return (
     <header className="nav-bar-container">
-      <img id="nav-bar-logo" src={window.logoURL} height="30px" width="30px" alt="Pinfluence Logo"/>
-      <div>
+
+
+      <Link to="/">
+        <div className="nav-bar-logo-container">
+          <img id="nav-bar-logo" src={window.logoURL} height="30px" width="30px" alt="Pinfluence Logo" />
+        </div>
+      </Link>
+
+      
+
+
+      <div className="search-bar">
         <i className="fas fa-search"></i>
-        <input type="text" placeholder="Search for Tiny House Design"/>
-        <button onClick={logout}>Log out</button>
+        <input className="search-input" type="text" placeholder="Search for Tiny House Design"/>
       </div>
+
+
+      <Link to="/"><div className="nav-bar-link">Home</div></Link>
+      <div className="nav-bar-link">Following</div>
+      <div className="nav-bar-link">{currentUser.email}</div>
+
+      <div className="vertical-line"></div>
+      <i className="fas fa-comment-dots inbox"></i>
+      <i className="fas fa-bell notifications"></i>
+      <button onClick={logout}>Log out</button>
     </header>
   );
 };
