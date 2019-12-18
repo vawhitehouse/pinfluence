cb4a090c6781134a63f887e44069551e31427809
Valerie Whitehouse <vawhitehouse@yahoo.com>
2019-12-18 08:55:22 -0500

    work on discover feed grid

--- frontend/components/pins/pin_index.jsx
+++ frontend/components/pins/pin_index.jsx
@@ -7,22 +7,48 @@ class PinIndex extends React.Component {
     super(props);
   }
 
+  
+
   componentDidMount() {
+    window.onload = this.resizeAllGridItems();
+    window.addEventListener("resize", this.resizeAllGridItems);
     this.props.fetchAllPins();
   }
+
+  resizeGridItem(gridItem) {
+    const grid = document.getElementsByClassName("grid")[0];
+
+    const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue("grid-auto-rows"));
+    const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue("grid-row-gap"));
+    const rowSpan = Math.ceil((gridItem.querySelector('.grid-item-content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+
+    gridItem.style.gridRowEnd = "span " + rowSpan;
+  }
+
+  resizeAllGridItems() {
+    const allItems = document.getElementsByClassName("grid-item");
+    for (let i = 0; i < allItems.length; i++) {
+      this.resizeGridItem(allItems[i]);
+    }
+  }
+
 
   render() {
     const { pins } = this.props;
     const allPins = pins.map(pin => (
       <PinIndexItem pin={pin} key={pin.id} />
     ))
+
+    // window.onload = this.resizeAllGridItems();
+    // window.addEventListener("resize", this.resizeAllGridItems);
+
     return ( 
       
       <div className="pin-index-container">
 
-        <ul className="index-grid">
+        <div className="grid">
           {allPins}
-        </ul>
+        </div>
 
         <button className="create-pin-button">
           <Link to="/create-pin" >
--- frontend/components/pins/pin_index_item.jsx
+++ frontend/components/pins/pin_index_item.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const PinIndexItem = props => (
-  <div className="pii-container">
-    <li className="pin-index-item" key={props.pin.id}>
+const PinIndexItem = props => {
+
+  
+  
+  return (
+  <div className="pii-container grid-item">
+    <div className="pin-index-item grid-item-content" key={props.pin.id}>
       <img src={props.pin.imageUrl} alt={props.pin.title} className="pii-image"/>
       <h4>{props.pin.title}</h4>
       {/* <Link to={`/pins/${props.pin.id}/edit`}>Edit</Link> */}
       {/* <button onClick={() => props.deletePin(props.pin.id)}>Delete</button> */}
-    </li>
+    </div>
   </div>
-);
+)};
 
 export default PinIndexItem;
